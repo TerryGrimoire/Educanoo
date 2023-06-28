e15408fd611fc0c56aae976e258c96251e2bc46e
unknown <terry.grimoire@gmail.com>
2023-06-28 17:24:39 +0400

    okok

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Header from "./components/Header/Header";
@@ -22,6 +23,9 @@ function App() {
     description:
       "Educanoo est une association loi 1901 qui agit depuis 2010 dans le domaine du développement solidaire. L’association développe ses activités sur l’ensemble du territoire de la Réunion depuis 2019 au cœur des quartiers prioritaires.",
   };
+
+  const [bandeau, setBandeau] = useState(true);
+
   return (
     <BrowserRouter>
       <HelmetProvider>
@@ -43,6 +47,34 @@ function App() {
           <Route path="/Contact" element={<Contact helmet={helmet} />} />
           <Route path="/Mentions" element={<Mentions />} />
         </Routes>
+        {bandeau && (
+          <div className="bandeau">
+            <h2>Cookie</h2>
+            <p>
+              Ce site est à usage informatif et n'utilise donc pas de cookie.
+              Nous ne collectons aucune donnée personnelle. Pour en savoir plus,
+              consultez notre{" "}
+              <a href="/mentions">politique de confidentialité</a>
+            </p>
+
+            <div>
+              <button
+                type="button"
+                className="button_style button_white_blue shad"
+                onClick={() => setBandeau(false)}
+              >
+                Tout accepter
+              </button>
+              <button
+                type="button"
+                className="button_style"
+                onClick={() => setBandeau(false)}
+              >
+                Tout refuser
+              </button>
+            </div>
+          </div>
+        )}
         <Prefooter />
         <Footer />
       </HelmetProvider>
